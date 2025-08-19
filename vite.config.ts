31756d4f0e3edd2e187ba23e246b9952432aa3e7
Mohamed Wael <103073614+MohamedWael3011@users.noreply.github.com>
2025-08-19 23:12:48 +0300

    Deployment fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/personal-portfolio",
+  base: "/personal-portfolio/",
   plugins: [react()],
   resolve: {
     alias: {
